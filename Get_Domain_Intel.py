2a36bcbe296c727af3458964e42d26a68ee2bc09
Makaveli-J <544301790@qq.com>
2022-03-10 16:16:58 +0800

    Update Get_Domain_Intel.py

--- Get_Domain_Intel.py
+++ Get_Domain_Intel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+import hashlib
 import wget
 
 #Set Sources of Threat Intelligence
@@ -9,3 +10,10 @@ TI_URLs = ['']
 #Download from URLs
 for i in TI_URLs:
     wget.download(i, out = '/etc/ti/domains/')
+
+new_file = 'newfile'
+old_file = 'old_file'
+
+#Check if file content updated
+new_hash = hashlib.md5(new_file.encode()).hexdigest()
+old_hash = hashlib.md5(old_file.encode()).hexdigest()
