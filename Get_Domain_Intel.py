2b6b4ac48b6f994ddea028fb498ad1928e0a116e
Makaveli-J <544301790@qq.com>
2022-03-11 10:30:36 +0800

    Update Get_Domain_Intel.py

--- Get_Domain_Intel.py
+++ Get_Domain_Intel.py
@@ -17,3 +17,4 @@ old_file = 'old_file'
 #Check if file content updated
 new_hash = hashlib.md5(new_file.encode()).hexdigest()
 old_hash = hashlib.md5(old_file.encode()).hexdigest()
+def filechk(new_file, old_file):
