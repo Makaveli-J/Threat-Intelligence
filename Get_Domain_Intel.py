9b9baf011755da7e4f613b23e16c274d361b87b4
Makaveli-J <544301790@qq.com>
2022-03-14 16:57:13 +0800

    Update Get_Domain_Intel.py

--- Get_Domain_Intel.py
+++ Get_Domain_Intel.py
@@ -4,12 +4,20 @@
 import hashlib
 import wget
 import TI_Sources
+import re
+import requests
 
 #Download from URLs
 for i in TI_Sources.TI_Domain:
     wget.download(i, out = '/etc/ti/domain/')
 for i in TI_Sources.TI_IP:
     wget.download(i, out = '/etc/ti/ip/')
+r = requests.get('')
+lst = r.content
+test = re.findall(r'<a[^>]* href="([^"]*)"', lst)
+for i in test:
+    if re.search(r'csv$', i) != None:
+        print(i)
 
 new_file = 'new_file'
 old_file = 'old_file'
